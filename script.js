0a790f3d8e8e247e7092c9bc2d27cfea33dda192
Pavan B <83314733+ValiantWind@users.noreply.github.com>
2022-09-22 16:39:22 +0000

    added license and changed something things

--- script.js
+++ script.js
@@ -2,18 +2,36 @@ var username = document.getElementById("username");
 var commentText = document.getElementById("commentText");
 var revealButton = document.getElementById("generate");
 var output = document.getElementById("output");
-// var file = document.getElementById("myFile");
-
-
+var pfp = document.getElementById("pfp");
 
 function generate(){
 
+	var myHeaders = new Headers();
+myHeaders.append("Authorization", "Client-ID {{clientId}}");
 
-fetch(`https://some-random-api.ml/canvas/youtube-comment?avatar=https://pasteboard.co/64prAodKCixt.png&username=a&comment=a`)
+var formdata = new FormData();
+formdata.append("image", pfp.files[0]);
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: formdata,
+  redirect: 'follow'
+};
+
+fetch("https://api.imgur.com/3/image", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+fetch(`https://some-random-api.ml/canvas/youtube-comment?avatar=https://cdn.discordapp.com/avatars/560789882738573324/bc220b0eeeabbe234026e4881f3c3b9c.png&username=${username.value}&comment=${commentText.value}`)
    .then(res => { 
 		console.log(res.url)
 		output.src = res.url
 		 
 
 })
+
+	
 }
